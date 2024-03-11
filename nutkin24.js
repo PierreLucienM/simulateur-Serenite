e4f74a4bcbe13fd8d76c05f317a0a11df0ca8d8e
Jean-Charles <jcbouchaud@42.fr>
2024-03-11 15:39:12 +0100

    fix duration

--- nutkin24.js
+++ nutkin24.js
@@ -742,7 +742,7 @@ const setMaxDurationListeners = () => {
     const maxDuration = Math.max(retirementAge - age, 0);    
     durationInput.max = maxDuration;
 
-    if (durationInput.value > maxDuration) {
+    if (durationInput.value > maxDuration || parseInt(durationInput.value) === 0) {
       durationInput.value = maxDuration
     }
     
